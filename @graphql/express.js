aa5030ce892a6959c9f11c12e290330a4177d70e
Sergey Zubov <serzbv@gmail.com>
2018-03-02 11:12:33 +0300

    #disable scurf

--- @graphql/express.js
+++ @graphql/express.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const csurf = require('csurf');
+// const csurf = require('csurf');
 const graphqlHTTP = require('express-graphql');
 
-const {HEADER, COOKIE} = require('packages/enum');
+// const {HEADER, COOKIE} = require('packages/enum');
 const schema = require('./schema/schema').default;
 
 const app = express();
@@ -12,13 +12,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const csrfProtetction = csurf({
-    cookie: {
-        key: COOKIE.csrf,
-        sameSite: true,
-    },
-    value: (req) => req.headers[HEADER.csrf],
-});
+// const csrfProtetction = csurf({
+//     cookie: {
+//         key: COOKIE.csrf,
+//         sameSite: true,
+//     },
+//     value: (req) => req.headers[HEADER.csrf],
+// });
 
 const graphqlServer = graphqlHTTP((req, res) => ({
     schema,
@@ -29,10 +29,11 @@ const graphqlServer = graphqlHTTP((req, res) => ({
 // Request from microservices
 app.use('/fromProtectedNetwork', graphqlServer);
 
-if (__PRODUCTION__) {
-    app.use('*', csrfProtetction, graphqlServer);
-} else {
-    app.use('*', graphqlServer); // to allow graph*i*Ql work
-}
+// if (__PRODUCTION__) {
+//     app.use('*', csrfProtetction, graphqlServer);
+// } else {
+//     app.use('*', graphqlServer); // to allow graph*i*Ql work
+// }
+app.use('*', graphqlServer); // to allow graph*i*Ql work
 
 module.exports = app;
